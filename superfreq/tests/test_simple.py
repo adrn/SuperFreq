4264bdd4a278a84a26beae205479d0d16ee9d1de
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-06-05 13:15:36 -0400

    test with a rolling window

--- superfreq/tests/test_simple.py
+++ superfreq/tests/test_simple.py
@@ -74,11 +74,29 @@ class SimpleBase(object):
             # create SuperFreq object for this time array
             sf = SuperFreq(t, p=self.p)
 
-            # try recovering the strongest frequency
+            # solve for the frequencies
             w,amp,phi = sf.frecoder(f[:sf.n], break_condition=1E-5)
             np.testing.assert_allclose(self.omega, w[:nfreq], atol=1E-8)
             np.testing.assert_allclose(self.A, amp[:nfreq], atol=1E-6)
             np.testing.assert_allclose(self.phi, phi[:nfreq], atol=1E-6)
+
+    def test_rolling_window(self):
+        ts = [np.linspace(0.+dd, 150.+dd, 42104) for dd in np.linspace(0,10,50)]
+        dws = []
+        for i,t in enumerate(ts):
+            logger.debug(i, t.min(), t.max(), len(t))
+            f = self.make_f(t)
+            nfreq = len(self.omega)
+
+            # create SuperFreq object for this time array
+            sf = SuperFreq(t, p=self.p)
+
+            # try recovering the strongest frequency
+            w,amp,phi = sf.frecoder(f[:sf.n], break_condition=1E-5)
+            dws.append(np.abs(self.omega - w[:nfreq]))
+
+        dws = np.array(dws)
+        assert np.all(np.abs(dws) < 1E-8)
 
 class TestSimple1(SimpleBase):
 
