c47e193ede9320248a9d879ec14894371723f199
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-06-05 15:57:05 -0400

    wat

--- superfreq/_naff.pyx
+++ superfreq/_naff.pyx
@@ -16,10 +16,10 @@ cdef extern from "math.h":
     double cos(double)
     double sin(double)
 
-cdef extern from "simpson.h":
+cdef extern from "cextern/simpson.h":
     double _simpson (double *y, double dx, int n)
 
-cdef extern from "brent.h":
+cdef extern from "cextern/brent.h":
     double local_min (double a, double b, double eps, double t,
                       double f(double x), double *x)
 
