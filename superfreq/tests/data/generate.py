7e21974237469b4f277a5c68a420a6dc4a8b0959
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-03-03 15:03:21 -0500

    test orbits in harmonic oscillator

--- superfreq/tests/data/generate.py
+++ superfreq/tests/data/generate.py
@@ -110,3 +110,83 @@ def get_isochrone_orbits(n_orbits=100, seed=42):
     f.close()
 
     return cache_file
+
+def get_harmonic_oscillator_orbits(n_orbits=100, seed=42):
+    np.random.seed(seed)
+
+    cache_file = os.path.abspath(_find_pkg_data_path("ho_orbits.h5"))
+
+    if os.path.exists(cache_file):
+        return cache_file
+
+    # integration parameters
+    nperiods = 50
+    nsteps_per_period = 256
+    nsteps = nperiods * nsteps_per_period
+
+    # ---------------------------------------------------------------
+    # first data set are orbits integrated in an Isochrone potential
+
+    # potential parameters
+    omegas = 2*np.pi / np.array([150., 71., 201.])
+    pot = gp.HarmonicOscillatorPotential(omega=omegas, units=galactic)
+
+    pos = np.zeros((3,n_orbits))
+    pos[:2] = np.random.uniform(-10, 10, size=(2,n_orbits))
+    pos = pos*galactic['length']
+
+    vel = np.zeros((3,n_orbits))*galactic['length']/galactic['time']
+
+    # compute true actions, true frequencies
+    w0 = gd.CartesianPhaseSpacePosition(pos=pos, vel=vel)
+    act,ang,frq = pot.action_angle(w0)
+    frq = np.repeat(frq[:,np.newaxis], axis=1, repeats=n_orbits)
+
+    # reshape down to 2d
+    act = act[:2]
+    ang = ang[:2]
+    frq = frq[:2]
+
+    true_periods = (2*np.pi / frq).max(axis=0)
+
+    # write to file
+    f = h5py.File(cache_file, "w")
+
+    truth = f.create_group("initial")
+    truth.create_dataset("actions", act.shape, dtype='f8', data=act.value)
+    truth["actions"].attrs['unit'] = str(act.unit)
+
+    truth.create_dataset("angles", ang.shape, dtype='f8', data=ang.value)
+    truth["angles"].attrs['unit'] = str(ang.unit)
+
+    truth.create_dataset("freqs", frq.shape, dtype='f8', data=frq.value)
+    truth["freqs"].attrs['unit'] = str(frq.unit)
+
+    # integrate them orbits -- have to do it this way to make sure
+    #   dt is right
+    all_x = np.zeros((2,nsteps+1,n_orbits))
+    all_v = np.zeros((2,nsteps+1,n_orbits))
+    all_t = np.zeros((nsteps+1,n_orbits))
+    for i,period in enumerate(true_periods):
+        print("Orbit {0}".format(i))
+        dt = period / nsteps_per_period
+        orbit = pot.integrate_orbit(w0[i], dt=dt, nsteps=nsteps,
+                                    Integrator=DOPRI853Integrator)
+        all_x[...,i] = orbit.pos.decompose(pot.units).value[:2]
+        all_v[...,i] = orbit.vel.decompose(pot.units).value[:2]
+        all_t[...,i] = orbit.t.decompose(pot.units).value
+
+    orb = f.create_group("orbits")
+    orb.create_dataset("t", all_t.shape, dtype='f8', data=all_t)
+    orb['t'].attrs['unit'] = str(pot.units['time'])
+
+    orb.create_dataset("x", all_x.shape, dtype='f8', data=all_x)
+    orb['x'].attrs['unit'] = str(pot.units['length'])
+
+    orb.create_dataset("v", all_v.shape, dtype='f8', data=all_v)
+    orb['v'].attrs['unit'] = str(pot.units['length']/pot.units['time'])
+
+    f.flush()
+    f.close()
+
+    return cache_file
